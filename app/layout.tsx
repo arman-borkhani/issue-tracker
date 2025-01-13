d53d80edafd6e0876ee2adef2541db182c92f9ea
arman-borkhani <armanborkhani@gmail.com>
2025-01-13 10:10:32 +0330

    Customize Radix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Theme>
+        <Theme accentColor="violet">
           <NavBar></NavBar>
           <main>
             <div className="container">{children}</div>
