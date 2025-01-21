9a35d362913b8e41c001cfb5ec5153ba4e5ce781
arman-borkhani <armanborkhani@gmail.com>
2025-01-21 09:29:09 +0330

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,42 +3,68 @@
 import { Button, Dialog, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch {
+      setError(true);
+    }
+  };
 
   return (
-    <Dialog.Root>
-      <Dialog.Trigger>
-        <Button color="red">Delete Issue</Button>
-      </Dialog.Trigger>
-      <Dialog.Content>
-        <Dialog.Title>Confirm Deletion</Dialog.Title>
-        <Dialog.Description mb="4">
-          Are you sure you want to delete this issue? This action can not be
-          undone.
-        </Dialog.Description>
-        <Flex gap="3" mt="4" justify="end">
-          <Dialog.Close>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </Dialog.Close>
-          <Dialog.Close>
-            <Button
-              onClick={async () => {
-                await axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
-              color="red"
-            >
-              Delete Issue
-            </Button>
-          </Dialog.Close>
-        </Flex>
-      </Dialog.Content>
-    </Dialog.Root>
+    <>
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </Dialog.Trigger>
+        <Dialog.Content>
+          <Dialog.Title>Confirm Deletion</Dialog.Title>
+          <Dialog.Description mb="4">
+            Are you sure you want to delete this issue? This action can not be
+            undone.
+          </Dialog.Description>
+          <Flex gap="3" mt="4" justify="end">
+            <Dialog.Close>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close>
+              <Button onClick={deleteIssue} color="red">
+                Delete Issue
+              </Button>
+            </Dialog.Close>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
+      <Dialog.Root open={error}>
+        <Dialog.Content>
+          <Dialog.Title>Error</Dialog.Title>
+          <Dialog.Description mb="4">
+            This isssue could not be deleted
+          </Dialog.Description>
+          <Flex mt="4" justify="end">
+            <Dialog.Close>
+              <Button
+                variant="soft"
+                color="gray"
+                onClick={() => setError(false)}
+              >
+                Ok
+              </Button>
+            </Dialog.Close>
+          </Flex>
+        </Dialog.Content>
+      </Dialog.Root>
+    </>
   );
 };
 
