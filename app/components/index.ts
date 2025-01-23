bf2c93922de395ec448bcb40661b107115a1550e
arman-borkhani <armanborkhani@gmail.com>
2025-01-23 04:57:39 -0500

    Build pagination component

--- app/components/index.ts
+++ app/components/index.ts
@@ -2,3 +2,4 @@ export { default as Link } from "./Link";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as IssueStatusBadge } from "./IssueStatusBadge";
 export { default as Skeleton } from "./Skeleton";
+export { default as Pagination } from "./Pagination";
