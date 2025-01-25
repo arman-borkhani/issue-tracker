494e79af567435a7d0d92871cbee0aedce628beb
arman-borkhani <armanborkhani@gmail.com>
2025-01-25 05:32:15 -0500

    Build the Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
-export default function Home() {
+import { prisma } from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import IssueChart from "./IssueChart";
+import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+
+const Home = async () => {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
   return (
-    <div>Home</div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Flex direction="column" gap="5">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+          <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        </Flex>
+      </Box>
+      <Box>
+        <LatestIssues />
+      </Box>
+    </Grid>
   );
-}
+};
+
+export default Home;
